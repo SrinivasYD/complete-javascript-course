1f97d3501ad9c120de9108e807742647b7026fa9
SrinivasYD <imyds1612@gmail.com>
2025-02-03 09:03:11 -0500

    values and variables exercise

---
+++ 01-Fundamentals-Part-1/practise/values and variables/exercise.js
@@ -0,0 +1,10 @@
+// 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// 2. Log their values to the console.
+
+let country = "India";
+let continent = "Asia";
+let population = "1.4 billion";
+console.log(country);
+console.log(continent);
+console.log(population);
