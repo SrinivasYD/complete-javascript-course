18dcc85ade6d218377e19a7345e7611b4a252b25
SrinivasYD <imyds1612@gmail.com>
2025-02-03 08:51:19 -0500

    learn variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -11,3 +11,21 @@ if (js === "amazing") {
 }
 
 console.log(40 + 8 + 23 - 10);
+
+// Variables
+let firstName = "Jonas";
+console.log(firstName);
+
+let $num = 5;
+console.log($num);
+
+let jonas_matilda = "JM";
+console.log(jonas_matilda);
+
+// We cannot use these name for variables
+
+// let 3years = 3; variable name cannot start with int
+// let jonas&matilda = 'JM'; variable name should not have ampersand
+// 'name' is a reserved variable in JS but still we can use it as a variable name
+// let Jonas = 'J'; variable name should not start with capital letter
+// let PI = 3.14; variable name with all uppercase letters is used for constant
